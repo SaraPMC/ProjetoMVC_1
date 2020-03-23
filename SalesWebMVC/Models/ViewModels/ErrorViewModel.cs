f8f6412bcd396d35ac6ceb90893a42b9bc6667ac
SaraPMC <sarapmc@hotmail.com>
2020-03-22 21:05:34 -0300

    First Model-Controller-View - Department

--- SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
@@ -1,7 +1,9 @@
 using System;
 
-namespace SalesWebMVC.Models
+namespace SalesWebMVC.Models.ViewModels
 {
+    //Não é um model nem uma entidade do negocio
+    //É apenas um modelo auxiliar para popular as telas
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
