d3ff591ed52872111478a06b9c43089847a392cd
SaraPMC <sarapmc@hotmail.com>
2020-03-22 22:56:02 -0300

    Other entities and second migration

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using SalesWebMVC.Models.Enums;
+using System;
 
 namespace SalesWebMVC.Models
 {
     public class SalesRecord
     {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
     }
 }
